765dc8a606797ee78f30eee1f261917732ec6991
Malik-94 <imenov-a@mail.ru>
2019-09-21 16:21:57 +0600

    первый этап

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('active', 'Активно'),
+    ('blocked', 'Заблокировано')
+]
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Имя автора')
+    mail = models.EmailField(verbose_name='Почта автора')
+    feedback_text = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Текст записи')
+    first_time = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    second_time = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    status = models.CharField(max_length=50, null=False, blank=False, choices=STATUS_CHOICES, default='active',
+                              verbose_name='Статус')
+
+    def __str__(self):
+        return self.name
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404
+from webapp.models import Book
 
-# Create your views here.
+
+def index_view(request):
+    # здесь нужно поставить правильный филтьр
+    # и сортировку вместо all()
+    #book = Book.objects.order_by('category', 'name')
+    book = Book.objects.all()
+    return render(request, 'index.html', context={
+        'book': book
+    })
+
